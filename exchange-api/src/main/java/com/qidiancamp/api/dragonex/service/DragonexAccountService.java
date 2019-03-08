d8659ea7cca957b555c1bbb28596adb21410ed39
chenyu <fastfish@163.com>
2019-03-08 10:45:25 +0800

    修改交易接口

--- exchange-api/src/main/java/com/qidiancamp/api/dragonex/service/DragonexAccountService.java
+++ exchange-api/src/main/java/com/qidiancamp/api/dragonex/service/DragonexAccountService.java
@@ -1,16 +1,15 @@
 package com.qidiancamp.api.dragonex.service;
 
 
-import com.qidiancamp.Exchange;
-import com.qidiancamp.api.dragonex.dto.account.Balance;
-import com.qidiancamp.currency.Currency;
-import com.qidiancamp.dto.account.AccountInfo;
-import com.qidiancamp.dto.account.Wallet;
-import com.qidiancamp.service.account.AccountService;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import com.qidiancamp.Exchange;
+import com.qidiancamp.currency.Currency;
+import com.qidiancamp.api.dragonex.dto.account.Balance;
+import com.qidiancamp.dto.account.AccountInfo;
+import com.qidiancamp.dto.account.Wallet;
+import com.qidiancamp.service.account.AccountService;
 
 public class DragonexAccountService extends DragonexAccountServiceRaw implements AccountService {
 
@@ -21,11 +20,11 @@ public class DragonexAccountService extends DragonexAccountServiceRaw implements
   @Override
   public AccountInfo getAccountInfo() throws IOException {
     List<Balance> userCoins = userCoins(exchange.getOrCreateToken().token);
-    List<Balance> balances =
+    List<com.qidiancamp.dto.account.Balance> balances =
         userCoins.stream()
             .map(
                 b ->
-                    new Balance(
+                    new com.qidiancamp.dto.account.Balance(
                         Currency.getInstance(b.code.toUpperCase()),
                         b.volume,
                         b.volume.subtract(b.frozen),
--- exchange-api/src/main/java/com/qidiancamp/api/dragonex/service/DragonexMarketDataService.java
+++ exchange-api/src/main/java/com/qidiancamp/api/dragonex/service/DragonexMarketDataService.java
@@ -2,13 +2,13 @@ package com.qidiancamp.api.dragonex.service;
 
 
 import com.qidiancamp.Exchange;
-import com.qidiancamp.api.dragonex.dto.marketdata.Ticker;
+import com.qidiancamp.api.dragonex.dto.marketdata.Order;
 import com.qidiancamp.currency.CurrencyPair;
-import com.qidiancamp.dto.Order;
+import com.qidiancamp.dto.Order.OrderType;
 import com.qidiancamp.dto.marketdata.OrderBook;
+import com.qidiancamp.dto.marketdata.Ticker;
 import com.qidiancamp.dto.trade.LimitOrder;
 import com.qidiancamp.service.marketdata.MarketDataService;
-
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +24,7 @@ public class DragonexMarketDataService extends DragonexMarketDataServiceRaw
 
   @Override
   public Ticker getTicker(CurrencyPair pair, Object... args) throws IOException {
-    Ticker t =
+    com.qidiancamp.api.dragonex.dto.marketdata.Ticker t =
         super.ticker(exchange.symbolId(pair)).get(0);
     return new Ticker.Builder()
         .currencyPair(pair)
@@ -41,15 +41,15 @@ public class DragonexMarketDataService extends DragonexMarketDataServiceRaw
   @Override
   public OrderBook getOrderBook(CurrencyPair pair, Object... args) throws IOException {
     long symbolId = exchange.symbolId(pair);
-    BiFunction<Order.OrderType, Order, LimitOrder> f =
+    BiFunction<OrderType, Order, LimitOrder> f =
         (t, o) -> new LimitOrder(t, o.volume, pair, null, null, o.price);
     List<LimitOrder> bids =
         super.marketBuyOrders(symbolId).stream()
-            .map(o -> f.apply(Order.OrderType.BID, o))
+            .map(o -> f.apply(OrderType.BID, o))
             .collect(Collectors.toList());
     List<LimitOrder> asks =
         super.marketSellOrders(symbolId).stream()
-            .map(o -> f.apply(Order.OrderType.ASK, o))
+            .map(o -> f.apply(OrderType.ASK, o))
             .collect(Collectors.toList());
     return new OrderBook(null, asks, bids);
   }
